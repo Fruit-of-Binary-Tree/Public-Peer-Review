649d45dae807f09e2fc9b28e76b05fa798e03dce
Aashna <2202103@students.wits.ac.za>
2022-09-06 22:26:04 +0200

    PostBox Tests

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,75 +1,75 @@
 import { signOut } from 'firebase/auth';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react'
-import {FaHome, FaSearch} from 'react-icons/fa'
-import {FiSettings} from 'react-icons/fi'
+import { FaHome, FaSearch } from 'react-icons/fa'
+import { FiSettings } from 'react-icons/fi'
 import { auth } from '../firebase-config';
 
 
 function Header() {
 
   //initialise variables
-    const [loginEmail, setLoginEmail] = useState ("");
-    const [loginPassword, setLoginPassword] = useState ("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
 
-    const [user, setUser] = useState({});
- 
-    const router = useRouter()
+  const [user, setUser] = useState({});
 
-    //logout function with Google Auth
-    const logout = async () => {
-    
-    
-        await signOut(auth)
-        .then((userCredential) => {
-          console.log("success");
-          router.push('/Login');  //On successful logout, user will be directed to login page
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-        });
-      };
-    
-    
-        
+  const router = useRouter()
+
+  //logout function with Google Auth
+  const logout = async () => {
+
+
+    await signOut(auth)
+      .then((userCredential) => {
+        console.log("success");
+        router.push('/Login');  //On successful logout, user will be directed to login page
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
+
+
   return (
     <div className='sticky top-0 z-50 flex justify-between bg-zinc-300 px-4 py-2 shadow-sm '>
-        <div className='text-left mx- 10 font-bold'>
-            {/*Title*/}
-            <span className='text-cyan-600 text-2xl'>Genius</span>
-            <span className='text-black text-2xl'>Reviews</span>
-        </div>
+      <div className='text-left mx- 10 font-bold'>
+        {/*Title*/}
+        <span className='text-cyan-600 text-2xl'>Genius</span>
+        <span className='text-black text-2xl'>Reviews</span>
+      </div>
 
-         {/* Home icon and styling used for refreshing page */}
-        <div className='text-cyan-600 flex items-center px-7 mr-8'>
-            <FaHome className='h-6 w-6'/>
-            <p className='ml-1 text-lg'>Home</p>
-        </div>
+      {/* Home icon and styling used for refreshing page */}
+      <div className='text-cyan-600 flex items-center px-7 mr-8'>
+        <FaHome className='h-6 w-6' />
+        <p className='ml-1 text-lg'>Home</p>
+      </div>
 
-        {/* Search box*/}
-        <div className='flex-grow'>
-            <form className='flex items-center space-x-2 border border-cyan-600 rounded-lg
+      {/* Search box*/}
+      <div className='flex-grow'>
+        <form className='flex items-center space-x-2 border border-cyan-600 rounded-lg
             bg-gray-100 py-1 px-3'>
-                <FaSearch className='text-cyan-600 h-6 w-10'/>
-                <input className='bg-transparent outline-none text-cyan-600' type='text' placeholder='Search Peer Review'/>
-                <button type='submit' hidden/>
-            </form>
+          <FaSearch className='text-cyan-600 h-6 w-10' />
+          <input className='bg-transparent outline-none text-cyan-600' type='text' placeholder='Search Peer Review' />
+          <button type='submit' hidden />
+        </form>
 
-        </div>
+      </div>
 
-        {/* Setting icon and styling used to edit user's details */}
-        <div className='flex items-center px-3 ml-8'>
-        <FiSettings className='h-6 w-6 text-cyan-600  '/>
-        </div>
+      {/* Setting icon and styling used to edit user's details */}
+      <div className='flex items-center px-3 ml-8'>
+        <FiSettings className='h-6 w-6 text-cyan-600  ' />
+      </div>
 
-         {/* Logout button and styling, onClick will call logout function */}
-        <div className='flex items-center px-2'>
-           <button onClick={logout} className='text-cyan-600 flex items-end text-lg' >Signout</button>  
-        </div>
-            
-        
-                
+      {/* Logout button and styling, onClick will call logout function */}
+      <div className='flex items-center px-2'>
+        <button onClick={logout} className='text-cyan-600 flex items-end text-lg' >Signout</button>
+      </div>
+
+
+
     </div>
 
   )
--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,6 +1,6 @@
-import React, { useState ,useCallback} from 'react'
-import {FaGoogle, FaRegEnvelope} from 'react-icons/fa'
-import {MdLockOutline} from 'react-icons/md'
+import React, { useState, useCallback } from 'react'
+import { FaGoogle, FaRegEnvelope } from 'react-icons/fa'
+import { MdLockOutline } from 'react-icons/md'
 import { auth, provider } from "../firebase-config"
 
 //imports for Google authentication
@@ -19,30 +19,30 @@ import { useRouter } from 'next/router'
 function Login() {
 
   //initializing variables
-  const [loginEmail, setLoginEmail] = useState ("");
-  const [loginPassword, setLoginPassword] = useState ("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
 
   const [user, setUser] = useState({});
   const router = useRouter()
 
   const login = async () => {
-    
+
     const auth = getAuth();
     signInWithEmailAndPassword(auth, loginEmail, loginPassword) //Manual sign in with users details from sign up page
       .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
         console.log("success"); //If successful login to home page
-        router.push('/Home'); 
-        
+        router.push('/Home');
+
 
         // ...
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message; //If unsuccessful error message will be displayed
-      }); 
-    
+      });
+
   };
 
   //On Google icon click the userr will be directed to gmail where they can enter their email and password to sign in with Google
@@ -53,74 +53,74 @@ function Login() {
     }).catch((error) => {
       console.log(error);
     });
-  };  
-  
-  {/*UI components*/}
+  };
+
+  {/*UI components*/ }
   return (
     <div className=" flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100
     w-full h-full bg-no-repeat bg-cover bg-[url('https://cdn.pixabay.com/photo/2016/03/09/15/29/books-1246674_1280.jpg')]"> {/*Background image and styling*/}
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center "> 
-      <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center ">
+        <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
           <div className="w-3/5 p-5">
             <div className='text-left font-bold'>
               {/*Title of website and styling*/}
               <span className='text-cyan-600 text-3xl'>Genius</span>
               <span className='text-black text-3xl'>Reviews</span>
+            </div>
+            <div className='py-7'>
+              <h2 className='text-3 font-bold mb-2 text-cyan-600'>Sign in to Account</h2>
+              <div className="border-2 w-10 inline-block border-cyan-600 mb-4"></div>
+              <div className='flex justify-center p-3 mb-1'>
+                {/*Button to initiate Google Auth*/}
+                <a href="#" onClick={signInWithGoogle} className='border-2 border-gray-200 rounded-full p-3 mx-1 mr-3'>
+                  <FaGoogle className='text-sm' />
+                </a>
+                <p className='px-4 py-2 inline-block '>Login with Google</p>
               </div>
-              <div className='py-7'>
-                <h2 className='text-3 font-bold mb-2 text-cyan-600'>Sign in to Account</h2>
-                <div className="border-2 w-10 inline-block border-cyan-600 mb-4"></div>
-                <div className='flex justify-center p-3 mb-1'>
-                  {/*Button to initiate Google Auth*/}
-                 <a href="#" onClick={signInWithGoogle} className='border-2 border-gray-200 rounded-full p-3 mx-1 mr-3'>
-                <FaGoogle className='text-sm'/>
-                </a>  
-                <p className='px-4 py-2 inline-block '>Login with Google</p>     
-            </div>
 
-            <div className='py-1 mb-3'>
-              <p className=''>Or sign in with email account</p>
-            </div>
+              <div className='py-1 mb-3'>
+                <p className=''>Or sign in with email account</p>
+              </div>
 
-            <div className='flex flex-col items-center'>
-                  <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
-                    {/* Email input*/}
-                    <FaRegEnvelope className='text-gray-400 mr-2'/>
-                    {/* Email input to be validated in firebase*/}
-                    <input type="email" name='email' placeholder='Email' onChange={(event) => {setLoginEmail(event.target.value)}} className='bg-gray-100 outline-none text-sm flex-1'></input>
-                  </div>
-                  <div className='bg-gray-100 w-64 p-2 flex items-center mb-4'>
-                    {/* Password input*/}
-                    <MdLockOutline className='text-gray-400 mr-2'/>
-                    {/* Password input to be validated in firebase*/}
-                    <input type="password" name='password' placeholder='Password'onChange={(event) => {setLoginPassword(event.target.value)}} className='bg-gray-100 outline-none text-sm flex-1'></input>
-                  </div>
-                  {/* Button to login and styling with hover effect, onClick event will validate information*/}
-                  <a href='#' onClick={login} className='text-cyan-600 border-2 border-cyan-600 rounded-full px-12 py-2 inline-block font-semibold 
-                    hover:bg-cyan-600 hover:text-white transition ease-out duration-500'>Login</a>    
-            </div> 
-            
+              <div className='flex flex-col items-center'>
+                <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+                  {/* Email input*/}
+                  <FaRegEnvelope className='text-gray-400 mr-2' />
+                  {/* Email input to be validated in firebase*/}
+                  <input type="email" name='email' placeholder='Email' onChange={(event) => { setLoginEmail(event.target.value) }} className='bg-gray-100 outline-none text-sm flex-1'></input>
+                </div>
+                <div className='bg-gray-100 w-64 p-2 flex items-center mb-4'>
+                  {/* Password input*/}
+                  <MdLockOutline className='text-gray-400 mr-2' />
+                  {/* Password input to be validated in firebase*/}
+                  <input type="password" name='password' placeholder='Password' onChange={(event) => { setLoginPassword(event.target.value) }} className='bg-gray-100 outline-none text-sm flex-1'></input>
+                </div>
+                {/* Button to login and styling with hover effect, onClick event will validate information*/}
+                <a href='#' onClick={login} className='text-cyan-600 border-2 border-cyan-600 rounded-full px-12 py-2 inline-block font-semibold 
+                    hover:bg-cyan-600 hover:text-white transition ease-out duration-500'>Login</a>
+              </div>
+
 
             </div>
-             
-            </div> {/*End of sign in section*/}
+
+          </div> {/*End of sign in section*/}
 
           <div className="w-2/5 bg-cyan-600 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12 ">
             <h2 className="text-3xl font-bold mb-2">Join our community!</h2>
             <div className="border-2 w-10 border-white inline-block mb-3"></div>
             <p className="mb-10 py-2 "> Fill in your details...</p>
             {/* Button to take the user to Signup page with styling*/}
-            <a href='./signup' 
+            <a href='./signup'
               className='border-2 border-white rounded-full px-10 py-2 inline-block font-semibold
                 hover:bg-white hover:text-cyan-600 transition ease-out duration-500'>Sign Up</a>
-            </div> {/*End of sign up section*/}
+          </div> {/*End of sign up section*/}
         </div>
 
       </main>
 
-  
-      
+
+
     </div>
   )
 }
