af0430b845c4e8dfad48b0706bcfb7d4197d2ab7
abby2412 <72755358+abby2412@users.noreply.github.com>
2022-09-13 15:46:26 +0200

    Update Post.tsx

--- pages/Post.tsx
+++ pages/Post.tsx
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {FaUniversity, FaUser, FaQuoteLeft, FaLink } from 'react-icons/fa'
 import {FiAlignJustify} from 'react-icons/fi'
 import { SiAcademia } from "react-icons/si";
+import { Alert } from 'react-bootstrap'
+import PaperDataService from "../services/paper.services"
 
 function Post() {
+
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setURL] = useState("");
+  const [description, setDescription] = useState("");
+  const [university, setUniversity] = useState("");
+  const [citations, setCitation] = useState("");
+  const [message, setMessage] = useState({ error: false, msg: "" });
+
+  const handleSubmit = async (e: { preventDefault: () => void })  => {
+    e.preventDefault();
+    if(title == "" || author == "" || url == "" || description == "" || university == "" || citations == ""){
+      setMessage({ error: true, msg: " all fiels are mandatory!"});
+      return;
+    }
+    const newPaper = {
+      title,
+      author,
+      url,
+      description,
+      university,
+      citations
+    }
+    console.log(newPaper);
+
+    try{
+      await PaperDataService.addPaper(newPaper);
+      setMessage({error: false, msg: " new book added!"});
+    }
+    catch (err){
+      //setMessage({error: true });
+    }
+
+    setTitle("");
+    setAuthor("");
+    setURL("");
+    setDescription("");
+    setCitation("");
+    setUniversity("");
+  };
+  
   return (
+    <form className='sticky flex-center top-16 z-50 border-gray-300 bg-zinc-200 p-2' onSubmit={handleSubmit}>
+    {message?.msg && (
+      <Alert>
+        {message?.error ? "danger" : "successs"}
+          {message?.msg}
+      </Alert>
+    )}
     <div className=" flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100
     w-full h-full bg-no-repeat bg-cover bg-[url('https://cdn.pixabay.com/photo/2016/03/09/15/29/books-1246674_1280.jpg')]">
     
@@ -20,32 +70,32 @@ function Post() {
 
                 <div className='bg-gray-100 w-96 p-2 flex items-center mb-3 rounded-md'>
                     <FaUser className='text-gray-400 mr-2'/>
-                    <input type="text" name='author' placeholder='Author' className='bg-gray-100 outline-none text-sm flex-1'></input>
+                    <input type="text" name='author' placeholder='Author' value = {author} onChange = {(e) => setAuthor(e.target.value)} className='bg-gray-100 outline-none text-sm flex-1'></input>
                 </div>
             
                 <div className='bg-gray-100 w-96 p-2 flex items-center mb-3 rounded-md'>
                     <FaUniversity className='text-gray-400 mr-2'/>
-                    <input type="text" name='university' placeholder='University' className='bg-gray-100 outline-none text-sm flex-1'></input>
+                    <input type="text" name='university' placeholder='University' value = {university} onChange = {(e) => setUniversity(e.target.value)} className='bg-gray-100 outline-none text-sm flex-1'></input>
                 </div>
 
                 <div className='bg-gray-100 w-96 p-2 flex items-center mb-3 rounded-md'>
                     <SiAcademia className='text-gray-400 align-center mr-2'/>
-                    <input type="text" name='title' placeholder='Title' className='bg-gray-100 outline-none text-sm flex-1'></input>
+                    <input type="text" name='title' placeholder='Title' value = {title} onChange = {(e) => setTitle(e.target.value)} className='bg-gray-100 outline-none text-sm flex-1'></input>
                 </div>
 
                 <div className='bg-gray-100 w-96 h-32 p-2 flex items-start mb-3 rounded-md'>
                     <FaQuoteLeft className='text-gray-400 mr-2'/>
-                    <input type="text" name='citations' placeholder='Citations' className='bg-gray-100 outline-none text-sm '></input>
+                    <input type="text" name='citations' placeholder='Citations' value = {citations} onChange = {(e) => setCitation(e.target.value)} className='bg-gray-100 outline-none text-sm '></input>
                 </div>
 
                 <div className='bg-gray-100 w-96 h-32 p-2 flex items-start mb-3 rounded-md'>
                     <FiAlignJustify className='h-5 w-5 text-gray-400 align-center mr-2'/>
-                    <input type="text" name='description' placeholder='Description of publication' className='bg-gray-100 outline-none text-sm flex-1'></input>
+                    <input type="text" name='description' placeholder='Description of publication' value = {description} onChange = {(e) => setDescription(e.target.value)} className='bg-gray-100 outline-none text-sm flex-1'></input>
                 </div>
 
                 <div className='bg-gray-100 w-96 h-16 p-2 flex items-start mb-3 rounded-md'>
                     <FaLink className='h-4 w-4 text-gray-400 align-center mr-2'/>
-                    <input type="text" name='url' placeholder='Url' className='bg-gray-100 outline-none text-sm flex-1'></input>
+                    <input type="text" name='url' placeholder='Url' value = {url} onChange = {(e) => setURL(e.target.value)} className='bg-gray-100 outline-none text-sm flex-1'></input>
                 </div>
 
                 <label className="text-center form-label w-64 px-44 text-white" form="customFile">OR</label>
@@ -54,13 +104,13 @@ function Post() {
                 type="file" id="customFile" />
                 
                 <div className='flex flex-row px-14'>
-                <a href='' 
+                <button type='submit'
                 className='border-2 mt-5 border-white rounded-full px-10 py-2 inline-block font-semibold mr-5 text-white
-                hover:bg-white hover:text-cyan-600 transition ease-out duration-500'>Submit</a>  
-
+                hover:bg-white hover:text-cyan-600 transition ease-out duration-500'>Submit</button>  
+                
                 <a href='./Home' 
                 className='border-2 mt-5 border-white rounded-full px-10 py-2 inline-block font-semibold text-white
-                hover:bg-white hover:text-cyan-600 transition ease-out duration-500'>Cancel</a>  
+                hover:bg-white hover:text-cyan-600 transition ease-out duration-500'>Home</a>  
                 </div>
 
                 
@@ -75,6 +125,7 @@ function Post() {
       </main>
    
     </div>
+    </form>
   )
 }
 
