39e271c189cf902d5ffc9d87d3dd9d73dea0b76a
abby2412 <72755358+abby2412@users.noreply.github.com>
2022-09-13 17:34:23 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,21 +2,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useRouter } from 'next/router'
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-/*const firebaseConfig = {
-  apiKey: "AIzaSyB-_wn3L8QSS336bqJN8fzbyU4x5pQZIkg",
-  authDomain: "project-peer-review-af22f.firebaseapp.com",
-  projectId: "project-peer-review-af22f",
-  storageBucket: "project-peer-review-af22f.appspot.com",
-  messagingSenderId: "362648112170",
-  appId: "1:362648112170:web:ba2cd984da8f1ddcfd0753",
-  measurementId: "G-YB23C0RH4Z"
-};*/
 
 const firebaseConfig = {
   apiKey: "AIzaSyAaeyN00ZUj8oUdEQAZp8XQ1yXbPmMa8oY",
@@ -29,6 +20,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
 
 export const auth = getAuth(app);
 
