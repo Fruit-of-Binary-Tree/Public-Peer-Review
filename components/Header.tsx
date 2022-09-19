f16e742eaef61fa3e9aca359d5803bbce3855642
Aashna <2202103@students.wits.ac.za>
2022-09-19 21:53:22 +0200

    Search Google Scholar API

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import { signOut } from 'firebase/auth';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react'
 //<<<<<<< HEAD
-import {FaHome, FaSearch} from 'react-icons/fa'
-import {FiSettings} from 'react-icons/fi'
+import { FaHome, FaSearch } from 'react-icons/fa'
+import { FiSettings } from 'react-icons/fi'
 import { auth } from '../firebase-config.js';
 //=======
 //import { FaHome, FaSearch } from 'react-icons/fa'
@@ -20,7 +20,12 @@ function Header() {
 
   const [user, setUser] = useState({});
 
+  const [keyWords, searchKeyWords] = useState('');
+
   const router = useRouter()
+
+
+
 
   //logout function with Google Auth
   const logout = async () => {
@@ -37,6 +42,28 @@ function Header() {
       });
   };
 
+
+  //Search Google Scholar Function
+  const search = (searchValue: any) => {
+    searchKeyWords(searchValue);
+
+    const SerpApi = require('google-search-results-nodejs');
+    const search = new SerpApi.GoogleSearch("7d67eaa0033c6aae1c4c233711f59dee820dabb5f34966d25f22ed319abace64");
+
+    const params = {
+      engine: "google_scholar",
+
+      // this should be the input from search box
+      q: searchValue
+    };
+
+    const callback = function (data: any) {
+      console.log(data["organic_results"]);
+    };
+
+    // Show result as JSON
+    search.json(params, callback);
+  }
 
 
   return (
@@ -58,7 +85,7 @@ function Header() {
         <form className='flex items-center space-x-2 border border-cyan-600 rounded-lg
             bg-gray-100 py-1 px-3'>
           <FaSearch className='text-cyan-600 h-6 w-10' />
-          <input className='bg-transparent outline-none text-cyan-600' type='text' placeholder='Search Peer Review' />
+          <input className='bg-transparent outline-none text-cyan-600' type='text' placeholder='Search Google Scholar' onChange={(e) => search(e.target.value)} />
           <button type='submit' hidden />
         </form>
 
