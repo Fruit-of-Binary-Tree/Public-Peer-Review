cbd3c8ef175cbb5592ba408fdacaae0cfef9ebfa
Aashna <2202103@students.wits.ac.za>
2022-09-26 11:50:07 +0200

    Added Create Post tests

--- __tests__/__tests__/CreatePost.test.tsx
+++ __tests__/__tests__/CreatePost.test.tsx
@@ -4,11 +4,35 @@ import { render, screen } from "@testing-library/react";
 import Post from "../../pages/CreatePost";
 import { unmountComponentAtNode } from "react-dom";
 
-describe('basic input component', () => {
-    it("Checking if post is created", () => {
-        const div = document.createElement("div");
+describe('CreatePost', () => {
+    test('Author Input', () => {
         render(<Post />);
-        unmountComponentAtNode(div);
+        expect(screen.getByPlaceholderText(/Author/i)).toBeVisible();
+    })
+
+    test('University Input', () => {
+        render(<Post />);
+        expect(screen.getByPlaceholderText(/University/i)).toBeVisible();
+    })
+
+    test('Title Input', () => {
+        render(<Post />);
+        expect(screen.getByPlaceholderText(/Title/i)).toBeVisible();
+    })
+
+    test('Citations Input', () => {
+        render(<Post />);
+        expect(screen.getByPlaceholderText(/Citations/i)).toBeVisible();
+    })
+
+    test('Description Input', () => {
+        render(<Post />);
+        expect(screen.getByPlaceholderText(/Description of publication/i)).toBeVisible();
+    })
+
+    test('Url Input', () => {
+        render(<Post />);
+        expect(screen.getByPlaceholderText(/Url/i)).toBeVisible();
     })
 
 })
