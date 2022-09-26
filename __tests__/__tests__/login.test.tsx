d1d68eb18dccfbdb28b06d0e4c06d2f6746387fd
Aashna <2202103@students.wits.ac.za>
2022-09-26 10:52:44 +0200

    Completed Login Tests

--- __tests__/__tests__/login.test.tsx
+++ __tests__/__tests__/login.test.tsx
@@ -6,19 +6,54 @@ import { unmountComponentAtNode } from "react-dom";
 import exp from "constants";
 
 describe('LoginPage', () => {
-    test("render the Login Page", () => {
+    test("render the text on the Login Page", () => {
         // render the login page
         render(<Login />);
         // expect there to be the following text: "Sign in to Account"
         expect(screen.getByText(/Sign in to Account/i)).toBeVisible();
+    })
+
+    test("render text on Login Page", () => {
+        render(<Login />);
         // expect there to be the following text: "Login with Google"
         expect(screen.getByText(/Login with Google/i)).toBeVisible();
-        // expect there to be the following text: "Or sign in with email account"
-        expect(screen.getByText(/Or sign in with email account/i)).toBeVisible();
+    })
 
+    test("render text on Login Page", () => {
+        render(<Login />);
+        // expect there to be the following text: "Login with Google"
+        expect(screen.getByText(/Or sign in with email account/i)).toBeVisible();
+    })
+
+    test("render text on Login Page", () => {
+        render(<Login />);
+        // expect there to be the following text: "Login with Google"
+        expect(screen.getByText(/Join our community!/i)).toBeVisible();
+    })
+
+    test("render text on Login Page", () => {
+        render(<Login />);
+        // expect there to be the following text: "Login with Google"
+        expect(screen.getByText(/Fill in your details.../i)).toBeVisible();
+    })
+
+    test("render text on Login Page", () => {
+        render(<Login />);
+        // expect there to be the following text: "Login with Google"
+        expect(screen.getByText(/Sign Up/i)).toBeVisible();
+    })
+
+
+
+    test("render inputboxes on Login Page", () => {
+        render(<Login />);
         // expect there to be the following input
-        //these are failing for some reason
-        //expect(screen.getByRole("textbox", { name: /email/i })).toBeVisible();
-        //expect(screen.getByRole("textbox", { name: /password/i })).toBeVisible();
+        expect(screen.getByPlaceholderText(/Email/i)).toBeVisible();
+    })
+
+    test("render inputboxes on Login Page", () => {
+        render(<Login />);
+        // expect there to be the following input
+        expect(screen.getByPlaceholderText(/Password/i)).toBeVisible();
     })
 })
