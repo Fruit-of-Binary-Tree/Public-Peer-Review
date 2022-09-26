09953b7306f379e28b80d99173468139dab40e90
abby2412 <72755358+abby2412@users.noreply.github.com>
2022-09-26 20:05:11 +0200

    Update List.jsx

--- components/List.jsx
+++ components/List.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import {collection, getDocs} from 'firebase/firestore'
+import {collection, getDocs, query} from 'firebase/firestore'
 import {db} from '../firebase-config'
+
+import { signOut } from 'firebase/auth';
+import { useRouter } from 'next/router';
+
+//<<<<<<< HEAD
+import {FaHome, FaSearch} from 'react-icons/fa'
+import {FiSettings} from 'react-icons/fi'
+import { auth } from '../firebase-config.js';
 
 export default function List() {
 
 
 const [papers, setPapers] = useState([])
+
+const [query, setQuery] = useState("");
 
 useEffect(() => {
     getPapers()
@@ -27,22 +37,40 @@ function getPapers(){
             setPapers(movs)
         })
         .catch(error => console.log(error.message))
-
-
 }
 
+
+
   return (
+
+    <div className='sticky top-0 z-50 flex justify-between bg-zinc-300 px-4 py-2 shadow-sm '>
+    {/* Search box*/}
+    <div className='flex-grow'>
+    <form className='flex items-center space-x-2 border border-cyan-600 rounded-lg
+        bg-gray-100 py-1 px-3'>
+      <FaSearch className='text-cyan-600 h-6 w-10' />
+      <input className='bg-transparent outline-none text-cyan-600' type='text' placeholder='Search Peer Review' onChange={(e) => setQuery(e.target.value)} />
+      <button type='submit' hidden />
+    </form>
+
+  </div>
+
     <div>
         <a href='#' onClick={() => getPapers()}
               className='border-2 text-cyan-600 border-cyan-600 rounded-md mt-2 px-12 py-2 inline-block font-semibold
               hover:bg-cyan-600 hover:text-white transition ease-out duration-500'>Refresh List?</a> 
       <h2> <b>List:</b></h2>
       <ul>
-        {papers.map(paper => (
+        {papers.filter((paper) => 
+          paper.data.author.toLowerCase().includes(query)
+          ).map(paper => (
             <li key={paper.id}> <b>Title:</b> {paper.data.title}<br/>
             &ensp; <b>Author:</b> {paper.data.author} | <b> URL:</b> {paper.data.url} </li> 
             ))}
       </ul>
     </div>
+
+    </div>
   )
 }
+
