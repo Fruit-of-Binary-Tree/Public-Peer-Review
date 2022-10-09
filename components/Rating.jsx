cbab7400b0db73caa9ce3d1bf4fd0ee7479cbc1e
abby2412 <72755358+abby2412@users.noreply.github.com>
2022-10-09 15:07:01 +0200

    Some sort of rating

--- components/Rating.jsx
+++ components/Rating.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactStars from 'react-rating-stars-component'
+import {FaStar} from "react-icons/fa"
 
-function Rating() {
+
+const StarRating = () => {
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+
   return (
     <div>
-        <ReactStars/>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+
+        return (
+          <label>
+            <input
+              type = "radio"
+              name="rating"
+              value={ratingValue}
+              onClick = {() => setRating(ratingValue)}
+            />
+            <FaStar
+              className="star"
+              color = {ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+            />  
+          </label>
+        )
+      })}
+      <p>Your rating is {rating}</p>
     </div>
   )
 }
 
-export default Rating
+export default StarRating
