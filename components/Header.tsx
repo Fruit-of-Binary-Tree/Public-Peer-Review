198e57badad555ac0940133008bd81ad69fbfe96
Quesera05 <quesera98@gmail.com>
2022-10-11 08:56:49 +0200

    Google Scholar

--- components/Header.tsx
+++ components/Header.tsx
@@ -46,40 +46,6 @@ function Header() {
   };
 
 
-  const handleChange = (event: any) => {
-    searchKeyWords(event.target.value);
-  }
-
-  const handleClick = (event: any) => {
-    event.preventDefault();
-    searchWords(keyWords);
-  }
-
-
-
-  //Search Google Scholar Function
-  const searchWords = async (searchValue: any) => {
-    searchKeyWords(searchValue);
-
-    const SerpApi = require('google-search-results-nodejs');
-    const search = new SerpApi.GoogleSearch("7d67eaa0033c6aae1c4c233711f59dee820dabb5f34966d25f22ed319abace64");
-
-    const params = {
-      engine: "google_scholar",
-      // query should be the input from search box
-      q: searchValue
-    };
-
-    const callback = function (data: any) {
-      console.log(data["organic_results"]);
-    };
-
-    // Show result as JSON
-    search.json(params, callback);
-
-  };
-
-
   return (
     <div className='sticky top-0 z-50 flex justify-between bg-zinc-300 px-4 py-2 shadow-sm '>
       <div className='text-left mx- 10 font-bold'>
@@ -94,19 +60,17 @@ function Header() {
         <p className='ml-1 text-lg font-semibold'>Home</p>
       </div>
 
-      {/* Search box*/}
-      <div className='flex-grow'>
-        <form className='flex items-center space-x-2 border border-cyan-600 rounded-lg
-            bg-gray-100 py-1 px-3'>
-          <FaSearch className='text-cyan-600 h-6 w-10' />
-          <input className='bg-transparent outline-none text-cyan-600' type='text' placeholder='Search Peer Review' onChange={handleChange} value={keyWords} />
-          <button type='submit' hidden onClick={handleClick} />
-        </form>
-      </div>
+    
 
       <div className='flex items-center '>
         <a href='./SearchPeerReview'
           className=' mr-5 ml-5 inline-block font-semibold text-cyan-600 text-lg  hover:text-black'>Search Peer Review</a>
+      </div>
+
+      {/*Internal search */}
+      <div className='flex items-center '>
+        <a href='./GoogleScholar'
+          className=' mr-5 ml-5 inline-block font-semibold text-cyan-600 text-lg  hover:text-black'>Search Google Scholar</a>
       </div>
 
       {/* Logout button and styling, onClick will call logout function */}
