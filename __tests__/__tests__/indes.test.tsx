1d9af6fa182e018119a7c241b4c8460eb2003451
abby2412 <72755358+abby2412@users.noreply.github.com>
2022-10-14 16:10:03 +0200

    Add files via upload

---
+++ __tests__/__tests__/indes.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Home from "../../pages/index";
+import { unmountComponentAtNode } from "react-dom";
+
+describe('basic input component', () => {
+    it("render without crashing", () => {
+        const div = document.createElement("div");
+        render(<Home />);
+        unmountComponentAtNode(div);
+    })
+
+})
