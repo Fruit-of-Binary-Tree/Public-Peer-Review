04e173272b351e9445bcc79b3839649e6fc010de
Quesera05 <quesera98@gmail.com>
2022-10-27 12:30:46 +0200

    Home button

--- pages/GoogleScholarHome.jsx
+++ pages/GoogleScholarHome.jsx
@@ -1,12 +1,14 @@
 import GoogleScholarFeed from '../components/GoogleScholarFeed';
 import React from 'react';
+import { FaHome, FaSearch } from 'react-icons/fa'
 
 function GoogleScholarHome() {
 
     return (
 
-        <div className='flex-row'>
-            <div className='flex items-center '>
+        <div className='sticky top-0 z-50 flex justify-between bg-zinc-300 px-4 py-2 shadow-sm '>
+            <div className='flex items-center text-cyan-600 font-semibold text-lg'>
+                <FaHome className='h-6 w-6 mr-1' />
                 <a href='./Home'>Home</a>
             </div>
             <GoogleScholarFeed />
