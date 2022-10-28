7f18d7b5a286f3a576d7d04c44bc92f4a4fa2b3b
abby2412 <72755358+abby2412@users.noreply.github.com>
2022-10-28 17:13:21 +0200

    Update Bibtex.tsx

--- pages/Bibtex.tsx
+++ pages/Bibtex.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 function Bibtex() {
   return (
-    <div className='px-5 py-5'>author= "Ritesh A"
+    <div className='px-5 py-5'>author= "Branden Ingram"
         <div className='py-1'>
-            title: "BCO"
+            title: "Play-style Identification through Deep Unsupervised Clustering of Trajectories"
             <div className='py-1'>
-                description: "NONE"
+                description: "In any game, play-style is a concept that describes the technique and strategy e"
                 <div className='py-1'>
-                    url: "https:www//"
+                    url: "https://ieee-cog.org/2022/assets/papers/paper_114.pdf"
                 </div>
             </div>
         </div>
